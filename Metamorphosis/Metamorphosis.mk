75fa32f11e5ff292334beff54f7c348da77c42f2
Blue Prawn <monnier.florent@gmail.com>
2014-04-22 00:17:16 +0200

    added "Metamorphosis" by Franz Kafka

---
+++ Metamorphosis/Metamorphosis.mk
@@ -0,0 +1,31 @@
+BASENAME = Metamorphosis
+DOCTOC = /usr/share/doc/groff-*/html/mom/toc.html
+
+ROFF = groff -mom
+PS2PDF = ps2pdf -dPDFSETTINGS=/ebook
+EDITOR = vim
+LINKS = links
+
+all: \
+  $(BASENAME).pdf \
+  #End
+
+%.ps: %.gm
+	$(ROFF) $< > $@
+
+%.pdf: %.ps
+	$(PS2PDF) $<
+
+.PHONY: edit
+edit:
+	$(EDITOR) $(BASENAME)*.gm
+
+.PHONY: readdoc
+readdoc:
+	$(LINKS) $(DOCTOC)
+
+.SECONDARY:
+clean:
+	$(RM) $(BASENAME)*.ps
+	$(RM) $(BASENAME)*.pdf
+
